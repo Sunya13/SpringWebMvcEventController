fd5bffe83d8dee7ef5d6cf065557b5b351e79af6
sunya <sunya_sayeed@yahoo.com>
2020-04-12 19:44:13 +0100

    base structure for spring

---
+++ src/test/java/sunya/projects/SpringWebMvcEvent/EventApp/EventAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sunya.projects.SpringWebMvcEvent.EventApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
